a867328348af88fb38da81055f066e96cd02feb5
HKHAN <hamykhan786@gmail.com>
2022-07-19 01:39:30 +0200

    Validation and Error Handling

--- WebApplicationMVC/Models/Movies.cs
+++ WebApplicationMVC/Models/Movies.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplicationMVC.Models
 {
@@ -7,10 +8,20 @@ namespace WebApplicationMVC.Models
         [Key]
         public Guid M_ID { get; set; }
         [Required]
+        [DisplayName("Movie Name")]
         public string Movie_Name { get; set; }
+        [Required]
+        [DisplayName("Release Date")]
         public DateTime Movie_ReleaseDate { get; set; }
+        [Required]
+        [DisplayName("Rating")]
+        [Range(1,5)]
         public int Movie_Rating { get; set; }
+        [Required]
+        [DisplayName("Genre")]
         public string Genre { get; set; }
+        [Required]
+        [DisplayName("Casts")]
         public string Casts { get; set; }
 
     }
